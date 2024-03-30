bc99bc1d10c13271035467c99df10935daa34d9a
AgentFabulous <kshitijgm@gmail.com>
2024-03-30 11:32:52 +0000

    build: Add Project Infinity X ASCII art on lunch
    Signed-off-by: tejas101k <tejassingh649@rediffmail.com>

--- envsetup.sh
+++ envsetup.sh
@@ -501,6 +501,10 @@ function _lunch_meat()
     if [[ -n "${CHECK_MU_CONFIG:-}" ]]; then
       check_mu_config
     fi
+
+    echo "";
+    cat $(gettop)/build/make/infinity_x_ascii_logo;
+    echo"";
 }
 
 function _lunch_usage()
